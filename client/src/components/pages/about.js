793c4622c6afb9305c73e5f4bfc955b29e4f9e91
Sully <aristotlepoultney@gmail.com>
2022-07-17 23:35:27 -0400

    added image assets and about page template

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import portrait from '../../assets/about-portrait.jpg';
+
 const about = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <section>
+        <img
+          className="about-portrait"
+          src={portrait}
+          alt="portrait of website author"
+        />
+        <h3>Hi, I'm Eve</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default about;
